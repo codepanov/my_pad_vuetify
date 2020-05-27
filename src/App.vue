5583425b7df189779b34c859aa66a666b6422979
codepanov@gmail.com <codepanov@gmail.com>
2020-05-27 18:49:51 +0200

    fixed addItem view

--- src/App.vue
+++ src/App.vue
@@ -85,10 +85,6 @@
 export default {
   name: 'App',
 
-  components: {
-   
-  },
-
   data: () => ({
     activeTab: 'home',
     sheet: false,
@@ -101,18 +97,18 @@ export default {
         { icon: 'mdi-web', title: 'Search the Web', num: 3 },
       ],
 
-      rules: [
-        // value => !!value || 'Required.',
-        // value => (value || '').length <= 20 || 'Max 20 characters',
-        value => {
-          const pattern = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
-          return pattern.test(value) || 'Invalid url.'
-        },
-      ],
-      images: [],
-      message: '',
-      url: '',
-      // diffbot: `http://api.diffbot.com/v3/image?&token=878c55163b7541c4455a7d7198468a70&url=https%3A//aleksandar.panov.rs`
+      // rules: [
+      //   // value => !!value || 'Required.',
+      //   // value => (value || '').length <= 20 || 'Max 20 characters',
+      //   value => {
+      //     const pattern = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+      //     return pattern.test(value) || 'Invalid url.'
+      //   },
+      // ],
+      // images: [],
+      // message: '',
+      // url: '',
+      // // diffbot: `http://api.diffbot.com/v3/image?&token=878c55163b7541c4455a7d7198468a70&url=https%3A//aleksandar.panov.rs`
   }),
   methods: {
     close_sheet(num) {
@@ -121,18 +117,18 @@ export default {
         window.location.href = 'https://google.com'
       }
     },
-    get_images() {
-      this.message = 'Wait, getting images'
-      axios.get(`http://api.diffbot.com/v3/image?&token=878c55163b7541c4455a7d7198468a70&url=${this.url}`)
-      .then((response) => {
-        console.log(response.data.objects)
-        this.images = response.data.objects
-        this.message = `Found ${response.data.objects.length} images. Check console!`
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
+    // get_images() {
+    //   this.message = 'Wait, getting images'
+    //   axios.get(`http://api.diffbot.com/v3/image?&token=878c55163b7541c4455a7d7198468a70&url=${this.url}`)
+    //   .then((response) => {
+    //     console.log(response.data.objects)
+    //     this.images = response.data.objects
+    //     this.message = `Found ${response.data.objects.length} images. Check console!`
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    // }
   }
 };
 </script>
--- src/views/AddItem.vue
+++ src/views/AddItem.vue
@@ -16,6 +16,36 @@ import Gallery from '../components/ImageGallery.vue'
 export default {
   components:{
     Gallery
+  },
+  data() {
+    return {
+      rules: [
+        // value => !!value || 'Required.',
+        // value => (value || '').length <= 20 || 'Max 20 characters',
+        value => {
+          const pattern = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+          return pattern.test(value) || 'Invalid url.'
+        },
+      ],
+      images: [],
+      message: '',
+      url: '',
+      // diffbot: `http://api.diffbot.com/v3/image?&token=878c55163b7541c4455a7d7198468a70&url=https%3A//aleksandar.panov.rs`
+    }
+  },
+  methods: {
+    get_images() {
+      this.message = 'Wait, getting images'
+      axios.get(`http://api.diffbot.com/v3/image?&token=878c55163b7541c4455a7d7198468a70&url=${this.url}`)
+      .then((response) => {
+        console.log(response.data.objects)
+        this.images = response.data.objects
+        this.message = `Found ${response.data.objects.length} images. Check console!`
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
   }
 }
 </script>
